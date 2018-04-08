2c296b71c7d68f9f61212ea9c77c152e9beca3c2
Ankit Srivastava <asri71@sapient.com>
2018-04-08 18:41:08 +0530

    Adding working code for dialogflow

---
+++ index.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+const geoCodeAPIURL = "maps.googleapis.com";
+const geoCodeAPIKey = "AIzaSyC5VSrxufQfaSaM6J-mfFQJgGfXpiAP-7w";
+const dealersHost = "ankitsrivastava-test.apigee.net";
+const dealersPath = "/stubdealersapi?size=5";
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/getDealers', function(req,res){
+  console.log("POST request accepted from dialogflow");
+  let city = req.body.result.parameters['geo-city'];
+  let location = req.body.result.parameters['Location'];
+  let locality = req.body.result.parameters['Locality'];
+  console.log("locality => " + locality);
+
+  if(locality){
+      getDealersOnBasisOfLocality(locality, function(error, finalResponse){
+        console.log("FinalResponse to Dialogflow " + finalResponse, error);
+          //var a = finalResponse;
+          res.setHeader("Content-type","application/json");
+          //res.send(JSON.stringify(a));
+          res.send(JSON.stringify({ 'speech': finalResponse, 'displayText': finalResponse }));
+      });
+
+  }
+});
+app.listen(3001);
+console.log("Listening on port 3001");
+
+
+function getDealersOnBasisOfLocality(locality, cb) {
+  return getCoordinates(locality, cb);
+  }
+
+
+function getCoordinates(locality, cb){
+
+  let path="";
+  let pathPrefix = "/maps/api/geocode/json?address=";
+  let pathSuffix = "&key=";
+  path = path + pathPrefix;
+  path = path + locality;
+  path = path + pathSuffix + geoCodeAPIKey;
+  var coordinatesPromiseResponse = getPromiseResponse(geoCodeAPIURL, path);
+  coordinatesPromiseResponse.then((output) => {
+    console.log(output)
+    if(output){
+        console.log("coordinatesResponse => " + output);
+      let response = JSON.parse(output);
+      let latitude = response['results'][0]['geometry']['location']['lat'];
+      let longitude = response['results'][0]['geometry']['location']['lng'];
+      getDealersNearCoordindates(latitude,longitude, cb)
+  }
+});
+}
+
+function getDealersNearCoordindates(latitude, longitude, cb) {
+
+    var requestURL = dealersPath + "&lat=" +  latitude + "&long=" + longitude;
+    var dealersPromiseReponse =  getPromiseResponse(dealersHost, requestURL);
+    dealersPromiseReponse.then((output) => {
+        console.log("dealersResponse => " + output);
+      output = JSON.parse(output);
+      let nearestDealer = output['dealers'][0];
+      let dealerName = nearestDealer['name'];
+      let distanceInKm = Math.round(nearestDealer['distance']['km']);
+      let finalResponse = "The nearest dealer to you is " + dealerName + " which is at a distance of " + distanceInKm +  " km away";
+      console.log("finalResponse => " + finalResponse);
+      cb(null, finalResponse)
+    });
+}
+
+function getPromiseResponse(host, path) {
+  return new Promise((resolve,reject) => {
+      https.get({host : host, path : path}, (res) => {
+        let body = "";
+        res.on('data', (d) => {body+=d;});
+        res.on('end', () => {
+          resolve(body);
+        });
+        res.on("error", (error) => {
+            console.log("Error occured is => " + error);
+          reject(error);
+        });
+      });
+  });
+}
